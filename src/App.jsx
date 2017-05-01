afb00017580c0b8da8d659747530c32e41a0fec6
Bruno O <hilt@outlook.com.br>
2017-05-01 11:13:16 -0300

    Add some things

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+const divide = (x) => {
+	return `.app .item { width: ${1 / x}% }`;
+}
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+	constructor(props) {
+		super();
+		this.state = {
+			count: 0
+		}
+	}
+
+	render() {
+		return (
+			<div className="App">
+				<style>{divide(this.state.count)}</style>
+				<button onClick={() => this.setState({
+					count: this.state.count + 1
+				})}>Rise up</button>
+				<section className="row">
+					{Array.from(Array(this.state.count).keys()).map((x) => {
+						return <button className="item" key={x.toString()}>{x}</button>;
+					})}
+				</section>
+			</div>
+		);
+	}
 }
 
 export default App;
